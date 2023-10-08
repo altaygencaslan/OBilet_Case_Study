51da8639962664e08c88b2e652767419ba0f84ed
altay.gencaslan <altay.gencaslan@defacto.com>
2023-10-09 02:27:57 +0300

    Created MemeoryCacheManager

---
+++ OBilet.Core/Cache/Abstract/IMemoryCacheManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OBilet.Core.Cache.Abstract
+{
+    public interface IMemoryCacheManager
+    {
+        public void SetWithExpire<T>(string key, T value, DateTime? expireDate = null);
+        public void TryGetValue<T>(string key, out T item);
+        public void Clear(string key);
+    }
+}
--- OBilet.Web/Controllers/BaseController.cs
+++ OBilet.Web/Controllers/BaseController.cs
@@ -1,8 +1,12 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Caching.Memory;
 using OBilet.Core.Business.Abstract;
+using OBilet.Core.Business.Concrete;
+using OBilet.Core.Cache.Abstract;
+using OBilet.Core.DTO.Base;
 using OBilet.Core.DTO.GetBusLocations;
 using OBilet.Core.DTO.GetSession;
+using OBilet.Integration.Services.Model.Base;
 using OBilet.Web.Models;
 using System;
 using System.Text.Json;
@@ -13,10 +17,14 @@ namespace OBilet.Web.Controllers
     public class BaseController : Controller
     {
         private readonly IHttpContextAccessor _contextAccessor;
+        private readonly IMemoryCacheManager _memoryCacheManager;
+        private readonly IOBiletManager _oBiletManager;
 
-        public BaseController(IHttpContextAccessor contextAccessor)
+        public BaseController(IHttpContextAccessor contextAccessor, IMemoryCacheManager memoryCacheManager, IOBiletManager oBiletManager)
         {
             _contextAccessor = contextAccessor;
+            _memoryCacheManager = memoryCacheManager;
+            _oBiletManager = oBiletManager;
         }
 
         public SessionRequestDto GetSessionDefault()
@@ -43,8 +51,7 @@ namespace OBilet.Web.Controllers
             };
         }
 
-
-        public BusLocationSearchModel GetFromCacheOrParam(int? _originId, int? _destinationId, DateTime? _departureDate, BusLocationsResponseDto[] _busLocations)
+        public BusLocationSearchModel GetDefaultsFromSessionOrParameters(int? _originId, int? _destinationId, DateTime? _departureDate, BusLocationsResponseDto[] _busLocations)
         {
             BusLocationSearchModel searchModel = new BusLocationSearchModel();
 
@@ -73,6 +80,31 @@ namespace OBilet.Web.Controllers
             return searchModel;
         }
 
+        public async Task<GeneralResponse<BusLocationsResponseDto[]>> GetBusLocationsTryGetCache(bool fromCache = true)
+        {
+            GeneralResponse<BusLocationsResponseDto[]>? busLocationsResponse = null;
+            if (fromCache)
+            {
+                _memoryCacheManager.TryGetValue("BusLocationsResponseDto", out busLocationsResponse);
+                if (busLocationsResponse != null)
+                {
+                    return busLocationsResponse;
+                }
+            }
 
+            busLocationsResponse = await _oBiletManager.GetBusLocationsAsync(new GeneralRequestDto<BusLocationRequestDto>
+            {
+                SessionRequest = GetSessionDefault(),
+                RequestItem =
+                new BusLocationRequestDto
+                {
+                    Date = DateTime.Now,
+                }
+            });
+
+            _memoryCacheManager.SetWithExpire("BusLocationsResponseDto", busLocationsResponse);
+
+            return busLocationsResponse;
+        }
     }
 }
